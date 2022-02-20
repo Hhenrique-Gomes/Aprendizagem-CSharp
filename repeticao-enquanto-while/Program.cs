2dd672febe64d0f0a756061eaffe006c85b4056c
Henrique99G <henriquegomes99h@gmail.com>
2022-02-20 14:03:34 -0300

    Adiconei um while principal para nunca terminar o programa

--- repeticao-enquanto-while/Program.cs
+++ repeticao-enquanto-while/Program.cs
@@ -11,24 +11,25 @@ namespace repeticao_enquanto_while
 
             double y = Convert.ToDouble(Console.ReadLine(),CultureInfo.InvariantCulture);
             double raiz = Math.Sqrt(y);
-
-            while(y >= 0.0)
+            while(y <= 0.0 || y > 0.0)
             {
-                Console.WriteLine(raiz.ToString("F3",CultureInfo.InvariantCulture));
-                Console.Write("Digite outro numero   ");
-                y = Convert.ToDouble(Console.ReadLine(),CultureInfo.InvariantCulture);
-                raiz = Math.Sqrt(y);
+                while (y >= 0.0)
+                {
+                    Console.WriteLine(raiz.ToString("F3", CultureInfo.InvariantCulture));
+                    Console.Write("Digite outro numero   ");
+                    y = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    raiz = Math.Sqrt(y);
+                }
 
+                while (y < 0.0)
+                {
+                    Console.WriteLine("Raiz quadrade de numero negativo não existe");
+                    Console.Write("Digite outro numero   ");
+                    y = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    raiz = Math.Sqrt(y);
+                }
             }
-
-            while (y < 0.0)
-            {
-                Console.WriteLine("Raiz quadrade de numero negativo não existe");
-                Console.Write("Digite outro numero   ");
-                y = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
-                raiz = Math.Sqrt(y);
-            }
-
+            Console.WriteLine("Codigo finalizado");
         }
     }
 }
