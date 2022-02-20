061e60e5e33314da07709f4aa853d44ec40b1eec
Henrique99G <henriquegomes99h@gmail.com>
2022-02-20 12:50:36 -0300

    Projeto calcula a raiz quadrada, mas falta validacao

---
+++ repeticao-enquanto-while/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace repeticao_enquanto_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um numero para saber a raiz   ");
+
+            double y = Convert.ToDouble(Console.ReadLine(),CultureInfo.InvariantCulture);
+            double raiz = Math.Sqrt(y);
+
+            while(y >= 0.0)
+            {
+                Console.WriteLine(raiz.ToString("F3",CultureInfo.InvariantCulture));
+                Console.Write("Digite outro numero   ");
+                y = Convert.ToDouble(Console.ReadLine(),CultureInfo.InvariantCulture);
+                raiz = Math.Sqrt(y);
+
+            }
+
+            while (y < 0.0)
+            {
+                Console.WriteLine("Raiz quadrade de numero negativo não existe");
+                Console.Write("Digite outro numero   ");
+                y = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+                raiz = Math.Sqrt(y);
+            }
+
+        }
+    }
+}
